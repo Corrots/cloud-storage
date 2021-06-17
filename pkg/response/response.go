7c0b5b089fa95e006247a0b8d62775d3211fcf6c
alex_yu <alex_yu@kingdee.com>
2021-06-17 14:03:39 +0800

    update pkg

---
+++ pkg/response/response.go
@@ -0,0 +1,45 @@
+package response
+
+import "github.com/corrots/cloud-storage/pkg/errors"
+
+const SuccessCode = 0
+
+const SuccessMsg = "ok"
+
+type Response struct {
+	ErrorCode   int         `json:"errcode"`
+	Description string      `json:"description"`
+	Data        interface{} `json:"data,omitempty"`
+}
+
+func New(data interface{}) *Response {
+	resp := Response{
+		ErrorCode:   SuccessCode,
+		Description: SuccessMsg,
+		Data:        data,
+	}
+	return &resp
+}
+
+func Fail(code int, desc string) *Response {
+	return &Response{
+		ErrorCode:   code,
+		Description: desc,
+		Data:        nil,
+	}
+}
+
+func Err(err error) *Response {
+	if e, ok := err.(errors.CodeError); ok {
+		return &Response{
+			ErrorCode:   e.Code(),
+			Description: e.Error(),
+			Data:        nil,
+		}
+	}
+
+	return &Response{
+		ErrorCode:   errors.InternalErrorCode,
+		Description: err.Error(),
+	}
+}
