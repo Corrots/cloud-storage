c4f7bd165c629e58d4d73dd7d8ae8e7f3d214a2a
alex_yu <alex_yu@kingdee.com>
2021-07-02 17:31:39 +0800

    test concurrency write

--- test/test1/main.go
+++ test/test1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"sync"
 	"time"
@@ -10,38 +11,38 @@ import (
 var mutex sync.Mutex
 
 func main() {
-	file, err := os.Create("./tmp/a.log")
+	file, err := os.Create("./tmp/a.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	rand.Seed(time.Now().UnixNano())
+
 	done := make(chan struct{}, 1)
 
-	//for {
-	//	select {
-	//	case <-timer.C:
-	//		return
-	//	case <-done:
-	//		return
-	//	default:
-	//		file.WriteString("main goroutine: \n")
-	//	}
-	//}
-
 	go func() {
-		//mutex.Lock()
+		mutex.Lock()
 		// file input
-		file.Write([]byte(time.Now().Format("2006-01-02 15:04:05")))
-
-		//mutex.Unlock()
-		done <- struct{}{}
-		fmt.Println("done")
+		file.Write([]byte(time.Now().Format("2006-01-02 15:04:05\n")))
+		mutex.Unlock()
 	}()
 
+	timer := time.NewTimer(2 * time.Second)
 	go func() {
-
+		for {
+			select {
+			case <-timer.C:
+				done <- struct{}{}
+				return
+			default:
+				mutex.Lock()
+				file.WriteString(time.Now().Format(time.RFC3339Nano) + "\n")
+				mutex.Unlock()
+			}
+		}
 	}()
 
 	<-done
+	fmt.Println("done")
 }
